df85c467ebf650b38ea808758dc3f79094358b27
xingHome <xingqq2233@qq.com>
2021-09-03 18:59:20 +0800

    优化实现逻辑

--- router/users/register.js
+++ router/users/register.js
@@ -36,6 +36,9 @@ register.use('/auth', async (req, res) => {
   if (doc) return res.send({ code: 403, msg: '该用户名已被占用' })
   let doc1 = await users.findOne({ email })
   if (doc1) return res.send({ code: 403, msg: '该邮箱已注册' })
+  if ((req.session.mailDate + (1000 * 60)) < Date.now()) {
+    return res.send({ code: 200, msg: '获取过于频繁' })
+  }
 
   let code = ""
   for (let i = 1; i <= 6; i++) {
@@ -57,13 +60,18 @@ register.use('/register', async (req, res) => {
   if (!/^[a-z0-9_-]{3,16}$/.test(userName)) return res.send({ code: 403, msg: '用户名有误' })
   if (!/^[a-z0-9_-]{6,18}$/.test(passWord)) return res.send({ code: 403, msg: '密码格式有误' })
   if (authCode != req.session.authCode) return res.send({ code: 403, msg: '验证码错误' })
+  req.session.authCode = null
   if (emailCode != req.session.mailCode) return res.send({ code: 403, msg: '邮箱验证码错误' })
   let doc = await users.findOne({ userName })
   if (doc) return res.send({ code: 403, msg: '该用户名已被占用' })
   let doc1 = await users.findOne({ email })
   if (doc1) return res.send({ code: 403, msg: '该邮箱已注册' })
+
+  console.log((req.session.mailDate + (1000 * 60)) < Date.now());
+  console.log((req.session.mailDate + (1000 * 60 * 5)) > Date.now());
+  console.log(req.session);
+
   if (
-    (req.session.mailDate + (1000 * 60)) < Date.now() &&
     (req.session.mailDate + (1000 * 60 * 5)) > Date.now()
   ) { } else {
     return res.send({ code: 403, msg: '验证码已过期' })
