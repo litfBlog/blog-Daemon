43d7273992d2b80762de61f25cb0ed51744e211d
xingHome <xingqq2233@qq.com>
2021-09-03 15:22:39 +0800

    优化实现逻辑

--- router/users/register.js
+++ router/users/register.js
@@ -9,6 +9,22 @@ const users = require('./../../modules/users')
 
 const sendMail = require("./../../modules/sendMail")
 
+register.use('/find', async (req, res) => {
+  let { userName, email } = req.body
+  console.log(req.body);
+  let doc
+  if (userName) {
+    doc = await users.findOne({ userName })
+  } else if (email) {
+    doc = await users.findOne({ email })
+  }
+  if (doc) {
+    res.send({ code: 200, status: 'no' })
+  } else {
+    res.send({ code: 200, status: "ok" })
+  }
+})
+
 // 已在 app.js 声明路由
 register.use('/auth', async (req, res) => {
   let { userName, email, passWord, authCode } = req.body
@@ -26,6 +42,7 @@ register.use('/auth', async (req, res) => {
     code += Math.floor(Math.random() * 10)
   }
   req.session.mailCode = code
+  req.session.mailDate = Date.now()
 
   sendMail(email, 'html', '用户注册-验证码', `
     您的验证码是 ${code}
@@ -45,6 +62,12 @@ register.use('/register', async (req, res) => {
   if (doc) return res.send({ code: 403, msg: '该用户名已被占用' })
   let doc1 = await users.findOne({ email })
   if (doc1) return res.send({ code: 403, msg: '该邮箱已注册' })
+  if (
+    (req.session.mailDate + (1000 * 60)) < Date.now() &&
+    (req.session.mailDate + (1000 * 60 * 5)) > Date.now()
+  ) { } else {
+    return res.send({ code: 403, msg: '验证码已过期' })
+  }
 
   users.create({
     userName, email, passWord
