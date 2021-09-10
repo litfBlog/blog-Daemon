43e5e19bd7ff2994733724dddf41508612a14b08
xingHome <xingqq2233@qq.com>
2021-09-10 09:27:26 +0800

    修改错误日志路径

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ log4js.configure({
     },
     error_file: {//：记录器4：输出到error log
       type: "dateFile",
-      filename: __dirname + `logs/${programName}_error`,//您要写入日志文件的路径
+      filename: __dirname + `/logs/${programName}_error`,//您要写入日志文件的路径
       alwaysIncludePattern: true,//（默认为false） - 将模式包含在当前日志文件的名称以及备份中
       // daysToKeep: 10,//时间文件 保存多少天，距离当前天daysToKeep以前的log将被删除
       //compress : true,//（默认为false） - 在滚动期间压缩备份文件（备份文件将具有.gz扩展名）
