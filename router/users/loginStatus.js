69248c6fdfae1dbac3c335788e8a9df234e72ef2
xingHome <xingqq2233@qq.com>
2021-11-10 18:44:47 +0800

    侧边栏阅读量

--- router/users/loginStatus.js
+++ router/users/loginStatus.js
@@ -11,11 +11,18 @@ const docs = require('./../../modules/docs')
 // 已在 app.js 声明路由
 loginStatus.use(async (req, res) => {
   if (req.session.isLogin) {
+    // 查询用户数据
     let user = await users.findOne({ _id: req.session.uid })
+    // 查询该用户的文章
     let doc = await docs.find({
       author: req.session.uid,
       status: 1
     }, { title: 0, info: 0, date: 0, author: 0, status: 0, _id: 0 })
+    // 统计累计阅读量
+    let views = 0
+    for (let i in doc) {
+      views += doc[i].views.length
+    }
     res.send({
       code: 200, data: {
         isLogin: true,
@@ -26,6 +33,8 @@ loginStatus.use(async (req, res) => {
         permission: req.session.permission,
         // 文章数量
         pagesNum: doc.length,
+        // 阅读量
+        viewsNum: views,
         // 点赞和粉丝 先写死
         fansNum: 0,
         loveNum: 0
