84e4c62a96938f9c99c3dd55e625de9b5ee5b2d7
xingHome <xingqq2233@qq.com>
2021-12-08 16:51:58 +0800

    移除无用模块

--- router/docs/findDocInIndex.js
+++ router/docs/findDocInIndex.js
@@ -2,17 +2,10 @@
  * @Author: litfa 
  * @Date: 2021-12-08 16:41:07 
  * @Last Modified by: litfa
- * @Last Modified time: 2021-12-08 16:41:51
+ * @Last Modified time: 2021-12-08 16:51:09
  */
 const router = require('express')()
 const docs = require('./../../modules/docs.js')
-
-const marked = require('marked')
-marked.setOptions({
-  highlight: function (code) {
-    return require('highlight.js').highlightAuto(code).value
-  }
-})
 
 router.use(async (req, res) => {
   // 第一页应该显示最后十条
